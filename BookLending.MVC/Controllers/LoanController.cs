e4474ab04aa227fc44cf024fc3688879b63f2555
Joselito Machado <paguelosantos@hotmail.com>
2024-05-22 10:40:31 -0300

    feat: visualizar cadastro no banco

---
+++ BookLending.MVC/Controllers/LoanController.cs
@@ -0,0 +1,22 @@
+﻿using BookLending.MVC.Data;
+using BookLending.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLending.MVC.Controllers;
+
+public class LoanController : Controller
+{
+    private readonly BookLendingDbContext _bookLendingDb;
+
+    public LoanController(BookLendingDbContext bookLendingDb)
+    {
+        _bookLendingDb = bookLendingDb;
+    }
+
+    public IActionResult Index()
+    {
+        IEnumerable<LoansModel> loans = _bookLendingDb.Loans;
+
+        return View(loans);
+    }
+}
---
+++ BookLending.MVC/Models/LoansModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookLending.MVC.Models;
+
+public class LoansModel
+{
+    public int Id { get; set; }
+    public string Recipient { get; set; } = string.Empty;
+    public string Provider { get; set; } = string.Empty;
+    public string BorrowedBook { get; set; } = string.Empty;
+    public DateTime LastUpdatedDate { get; set; } = DateTime.Now;
+}
