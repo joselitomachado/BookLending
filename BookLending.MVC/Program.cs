3e9c49ffad88ceae37fe7330a9e96d0e32bb0dc5
Joselito Machado <paguelosantos@hotmail.com>
2024-05-22 08:18:23 -0300

    feat: conexão com o banco de dados

--- BookLending.MVC/Program.cs
+++ BookLending.MVC/Program.cs
@@ -1,7 +1,15 @@
+using BookLending.MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BookLendingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookLendingCs"));
+});
 
 var app = builder.Build();
 
