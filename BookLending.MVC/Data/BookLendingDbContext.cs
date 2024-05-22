4aada9f4a2c2d565456bf27246f923880c4f63ec
Joselito Machado <paguelosantos@hotmail.com>
2024-05-22 10:39:33 -0300

    feat: criação da tabela no banco de dados

---
+++ BookLending.MVC/Data/BookLendingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookLending.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLending.MVC.Data;
+
+public class BookLendingDbContext : DbContext
+{
+    public BookLendingDbContext(DbContextOptions<BookLendingDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<LoansModel> Loans { get; set; }
+}
