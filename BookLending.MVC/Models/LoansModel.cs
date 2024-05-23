b8536df29100447a4e90420717207ac0c7c6bd6b
Joselito Machado <paguelosantos@hotmail.com>
2024-05-23 08:31:10 -0300

    feat: add campo obrigatório no form

--- BookLending.MVC/Models/LoansModel.cs
+++ BookLending.MVC/Models/LoansModel.cs
@@ -1,10 +1,15 @@
-﻿namespace BookLending.MVC.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLending.MVC.Models;
 
 public class LoansModel
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Digite o nome do Recebedor")]
     public string Recipient { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite o nome do Fornecedor")]
     public string Provider { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite o nome do Livro")]
     public string BorrowedBook { get; set; } = string.Empty;
     public DateTime LastUpdatedDate { get; set; } = DateTime.Now;
 }
