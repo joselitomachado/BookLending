7ed9d7c40f4cbd1722dfeb280a954e026d0526ef
Joselito Machado <paguelosantos@hotmail.com>
2024-05-23 08:32:24 -0300

    feat: add função para registrar, editar e excluir os empréstimos

--- BookLending.MVC/Controllers/LoanController.cs
+++ BookLending.MVC/Controllers/LoanController.cs
@@ -13,10 +13,96 @@ public class LoanController : Controller
         _bookLendingDb = bookLendingDb;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         IEnumerable<LoansModel> loans = _bookLendingDb.Loans;
 
         return View(loans);
     }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        LoansModel loan = _bookLendingDb.Loans.FirstOrDefault(x => x.Id == id);
+
+        if(loan == null)
+        {
+            return NotFound();
+        }
+
+        return View(loan);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        LoansModel loan = _bookLendingDb.Loans.FirstOrDefault(x => x.Id == id);
+
+        if (loan == null)
+        {
+            return NotFound();
+        }
+
+        return View(loan);
+    }
+
+    [HttpPost]
+    public IActionResult Register(LoansModel loans)
+    {
+        if (ModelState.IsValid)
+        {
+            _bookLendingDb.Loans.Add(loans);
+            _bookLendingDb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Edit(LoansModel loan)
+    {
+        if (ModelState.IsValid)
+        {
+            _bookLendingDb.Loans.Update(loan);
+            _bookLendingDb.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(loan);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(LoansModel loan)
+    {
+        if(loan == null)
+        {
+            return NotFound();
+        }
+
+        _bookLendingDb.Loans.Remove(loan);
+        _bookLendingDb.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+
 }
