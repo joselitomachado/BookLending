3691634b2e612f8f4a3213cfa03003b9362eb2e7
Joselito Machado <paguelosantos@hotmail.com>
2024-05-26 00:09:03 -0300

    feat: alterar o datetime

--- BookLending.MVC/Models/LoansModel.cs
+++ BookLending.MVC/Models/LoansModel.cs
@@ -11,5 +11,5 @@ public class LoansModel
     public string Provider { get; set; } = string.Empty;
     [Required(ErrorMessage = "Digite o nome do Livro")]
     public string BorrowedBook { get; set; } = string.Empty;
-    public DateTime LastUpdatedDate { get; set; } = DateTime.Now;
+    public DateTime LoanDate { get; set; }
 }
