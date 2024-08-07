d60673fda8298f1f3c4e4b49331a5aa2eb5b15e1
Joselito Machado <paguelosantos@hotmail.com>
2024-08-07 08:13:35 -0300

    DTO de registrar usuario

---
+++ BookLending.MVC/DTOs/UserRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLending.MVC.DTOs;
+
+public class UserRegisterDto
+{
+    [Required(ErrorMessage ="Digite o nome")]
+    public string Name { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite o sobrenome")]
+    public string LastName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite o email")]
+    public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite a senha")]
+    public string Password { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite a confirmação de senha"),
+        Compare("Password", ErrorMessage = "As senha não estão iguais.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
