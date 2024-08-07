a21d252fa3f4437c8519637f1fadad6d5bb70702
Joselito Machado <paguelosantos@hotmail.com>
2024-08-07 08:13:44 -0300

    new Model

---
+++ BookLending.MVC/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookLending.MVC.Models;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
+}
