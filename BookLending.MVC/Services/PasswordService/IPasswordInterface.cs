041b943434343b41a0db24b8fe05e30db2e78bc3
Joselito Machado <paguelosantos@hotmail.com>
2024-08-07 08:14:11 -0300

    add senha interface

---
+++ BookLending.MVC/Services/PasswordService/IPasswordInterface.cs
@@ -0,0 +1,6 @@
+﻿namespace BookLending.MVC.Services.PasswordService;
+
+public interface IPasswordInterface
+{
+    void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+}
