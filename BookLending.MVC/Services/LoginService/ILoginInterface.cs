411ca44c6098b702d2b2bde8ef2b83588f9e94e9
Joselito Machado <paguelosantos@hotmail.com>
2024-08-07 08:13:57 -0300

    add login interface

---
+++ BookLending.MVC/Services/LoginService/ILoginInterface.cs
@@ -0,0 +1,9 @@
+﻿using BookLending.MVC.DTOs;
+using BookLending.MVC.Models;
+
+namespace BookLending.MVC.Services.LoginService;
+
+public interface ILoginInterface
+{
+    Task<ResponseModel<UserModel>> UserRegister(UserRegisterDto userRegisterDto);
+}
