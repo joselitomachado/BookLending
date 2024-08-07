c3c8db002ee458e588ca273bfc4f822e6a3b1a86
Joselito Machado <paguelosantos@hotmail.com>
2024-08-07 08:14:18 -0300

    add senha service

---
+++ BookLending.MVC/Services/PasswordService/PasswordService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace BookLending.MVC.Services.PasswordService;
+
+public class PasswordService : IPasswordInterface
+{
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
