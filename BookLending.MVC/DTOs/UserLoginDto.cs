1c92883bffe571ad54e2ce87a9b4134bc9a0fc8d
Joselito Machado <paguelosantos@hotmail.com>
2024-08-09 09:49:28 -0300

    add data annotations

---
+++ BookLending.MVC/DTOs/UserLoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLending.MVC.DTOs;
+
+public class UserLoginDto
+{
+    [Required(ErrorMessage = "Digite o e-mail")]
+    public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Digite a senha")]
+    public string Password { get; set; } = string.Empty;
+}
