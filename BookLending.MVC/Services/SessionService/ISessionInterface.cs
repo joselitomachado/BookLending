296798203e28bf9144817a47575ef5f3671693a0
Joselito Machado <paguelosantos@hotmail.com>
2024-08-09 09:50:31 -0300

    feat: adicionar sessão

---
+++ BookLending.MVC/Services/SessionService/ISessionInterface.cs
@@ -0,0 +1,10 @@
+﻿using BookLending.MVC.Models;
+
+namespace BookLending.MVC.Services.SessionService;
+
+public interface ISessionInterface
+{
+    UserModel GetSession();
+    void CreateSession(UserModel userModel);
+    void RemoveSession();
+}
---
+++ BookLending.MVC/Services/SessionService/SessionService.cs
@@ -0,0 +1,36 @@
+﻿using BookLending.MVC.Models;
+using Newtonsoft.Json;
+
+namespace BookLending.MVC.Services.SessionService;
+
+public class SessionService : ISessionInterface
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public SessionService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    public UserModel GetSession()
+    {
+        var sessionUser = _contextAccessor.HttpContext.Session.GetString("sessionUser");
+
+        if (string.IsNullOrEmpty(sessionUser))
+            return null;
+
+        return JsonConvert.DeserializeObject<UserModel>(sessionUser);
+    }
+
+    public void CreateSession(UserModel userModel)
+    {
+        var userJson = JsonConvert.SerializeObject(userModel);
+
+        _contextAccessor.HttpContext.Session.SetString("sessionUser", userJson);
+    }
+
+    public void RemoveSession()
+    {
+        _contextAccessor.HttpContext.Session.Remove("sessionUser");
+    }
+}
