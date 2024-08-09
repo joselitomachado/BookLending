c2f7aa1d5fba12a26ca383f89e7996dbd69daaa3
Joselito Machado <paguelosantos@hotmail.com>
2024-08-09 09:50:17 -0300

    feat: verificar senha

--- BookLending.MVC/Services/PasswordService/IPasswordInterface.cs
+++ BookLending.MVC/Services/PasswordService/IPasswordInterface.cs
@@ -3,4 +3,5 @@
 public interface IPasswordInterface
 {
     void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+    bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt);
 }
--- BookLending.MVC/Services/PasswordService/PasswordService.cs
+++ BookLending.MVC/Services/PasswordService/PasswordService.cs
@@ -12,4 +12,13 @@ public class PasswordService : IPasswordInterface
             passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
         }
     }
+
+    public bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
 }
